c985763753152f2b5b65fa1552174c09d68e54c8
Bobin_Vyacheslav <bobin.vyacheslav@yandex.ru>
2025-09-20 12:01:03 +0300

    add MainClass

---
+++ MavenEducation/src/main/java/MainClass.java
@@ -0,0 +1,12 @@
+public class MainClass {
+    private int num;
+    private String line;
+    protected static double irrationalNumber;
+    public final long bigNumber = 12345678;
+    public static void main(String[] args) {
+        for (int i = 0; i<=15; i++) {
+            System.out.println("Iter: " + i);
+        }
+
+    }
+}
