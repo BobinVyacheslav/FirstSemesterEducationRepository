0184e349908daf034e2f12c8d461a2cd5d00b82c
Bobin_Vyacheslav <bobin.vyacheslav@yandex.ru>
2025-10-12 16:43:32 +0300

    add CollectionUtils tests

---
+++ MavenEducation/src/test/java/com/mipt/vyacheslavbobin/genericsHomework/CollectionUtilsTest.java
@@ -0,0 +1,41 @@
+package com.mipt.vyacheslavbobin.genericsHomework;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollectionUtilsTest {
+  @Test
+  public void testAddAllArrayList() {
+    ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(8, 3, 5, 4, 9));
+    ArrayList<Integer> empty = new ArrayList<>();
+    CollectionUtils.addAll(list1, list2);
+    ArrayList<Integer> answ = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 8, 3, 5, 4, 9));
+    assertEquals(list1, answ);
+    CollectionUtils.addAll(list1, empty);
+    assertEquals(list1, answ);
+  }
+  @Test
+  public void testAddAllLinkedList() {
+    LinkedList<Integer> linkedList1 = new LinkedList<>(Arrays.asList(2, 9));
+    LinkedList<Integer> linkedList2 = new LinkedList<>(Arrays.asList(7, 8));
+    LinkedList<Integer> answ = new LinkedList<>(Arrays.asList(2, 9, 7, 8));
+    CollectionUtils.addAll(linkedList1, linkedList2);
+    assertEquals(linkedList1, answ);
+  }
+  @Test
+  public void testMergeLists() {
+    List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
+    List<Integer> list2 = Arrays.asList(8, 3, 5, 4, 9);
+    List<Integer> answ = Arrays.asList(1, 2, 3, 4, 5, 8, 3, 5, 4, 9);
+    List<Integer> merged = CollectionUtils.mergeLists(list1, list2);
+    assertEquals(merged, answ);
+
+  }
+}
